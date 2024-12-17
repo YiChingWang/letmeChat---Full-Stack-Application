4cd20328e23fc98df439474e75c62c771ae39773
Ollie <wang.yichi@northeastern.edu>
2024-12-17 12:43:51 -0600

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import MainPage from "./components/mainPage/mainPage";
+import MainPage from "./components/MainPage/MainPage";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 import "./index.css";
